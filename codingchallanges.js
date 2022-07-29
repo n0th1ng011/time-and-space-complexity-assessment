80f138d9effb54570d22b3ad48671c8af016ad38
Po <po@Pos-MacBook-Pro.localdomain>
2022-07-29 12:44:41 -0600

    Working on Challenges

---
+++ codingchallanges.js
@@ -0,0 +1,22 @@
+// 1. Sum Zero
+let nums = [1, 3, 9, 5, 18, -5, 42]
+
+for(let i = 0; i < nums.length; i++) {
+    for(let y = 0; y < nums.length; y++) {
+         {
+            if(nums[y] + nums[i] === 0) {
+                console.log(true)
+            }
+        }
+    }
+}
+
+// 2. Unique Characters
+function hasUniqueChars(str){
+    return new Set(str).size === str.length
+}
+console.log(hasUniqueChars('cat'))
+
+// 3. Pengram Sentance
+
+// 4. Longest Word
