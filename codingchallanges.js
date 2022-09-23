078a684d3b2ea98acb435464dc4487daa9f8f017
Po <po@Pos-MacBook-Pro.local>
2022-09-23 00:48:49 -0700

    Finished last two challenges

--- codingchallanges.js
+++ codingchallanges.js
@@ -11,6 +11,22 @@ for(let i = 0; i < nums.length; i++) {
     }
 }
 
+// Review
+const addToZero = (array) => {
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(i !== j){
+                if(array[i] = array[j] === 0){
+                    console.log(true)
+                    return
+                }
+            }
+        }
+    }
+    console.log(false)
+    return
+}
+
 // 2. Unique Characters
 function hasUniqueChars(str){
     return new Set(str).size === str.length
@@ -19,4 +35,31 @@ console.log(hasUniqueChars('cat'))
 
 // 3. Pengram Sentance
 
+
+const isPangram = (str) => {
+    let pangramArray = str.toLowerCase().replace(/[^a-z]+/g, '').split('')
+    let pangramSet = new Set()
+
+    for(let i = 0; i < pangramArray.length; i++){
+        pangramSet.add(pangramArray[i])
+    }
+    if(pangramSet.size === 26){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+
+}
+
 // 4. Longest Word
+
+const findLongestWord = (arr) => {
+    let longestCount = 0 
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].length > longestCount){
+            longestCount = arr[i].length
+        }
+    }
+    console.log(longestCount)
+}
